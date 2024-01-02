254b74796322da4b865c49ca7138d3f88dac519e
khanhgahg080-3486 <nirob.auc@gmail.com>
2024-01-03 02:15:41 +0800

    StaffModel

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'staff_name',
+        'email',
+        'phone',
+        'designation',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
